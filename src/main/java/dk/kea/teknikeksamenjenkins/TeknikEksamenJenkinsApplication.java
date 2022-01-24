bd75e98df8fbca61a246f4b64c07aa46fba03735
Delamir <td_christian@hotmail.com>
2022-01-24 20:21:40 +0100

    Initial commit

---
+++ src/main/java/dk/kea/teknikeksamenjenkins/TeknikEksamenJenkinsApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.teknikeksamenjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknikEksamenJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeknikEksamenJenkinsApplication.class, args);
+    }
+
+}
